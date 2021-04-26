58b51dbb49a9e267c507d6e1222fa69a7d01a197
Jeff Lebowski <anentitymusic@gmail.com>
2021-04-26 19:55:22 -0400

    set up variables in js

---
+++ script.js
@@ -0,0 +1,10 @@
+var city="";
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty = $("#humidity");
+var currentWSpeed =$("#wind-speed");
+var currentUvindex = $("#uv-index");
+var APIKey = "5391cd1c9cda260fb72d17ac7e4e7609";
