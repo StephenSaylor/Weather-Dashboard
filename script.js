b77bdf57c80dcadc4b4b9b33a628bb15bd8bf38f
Jeff Lebowski <anentitymusic@gmail.com>
2021-04-26 21:40:00 -0400

    uv index url and function

--- script.js
+++ script.js
@@ -7,4 +7,27 @@ var currentTemperature = $("#temperature");
 var currentHumidty = $("#humidity");
 var currentWSpeed =$("#wind-speed");
 var currentUvindex = $("#uv-index");
-var APIKey = "5391cd1c9cda260fb72d17ac7e4e7609";
+var sCity=[];
+var APIKey = "5391cd1c9cda260fb72d17ac7e4e7609"; 
+
+
+
+
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+
+function UVIndex(ln,lt){
+    //lets build the url for uvindex.
+    var UVIndexUrl="https://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey + "&lat=" +lt+ "&lon=" +ln;
+    $.ajax({
+            url: UVIndexUrl,
+            method:"GET"
+            }).then(function(response){
+                $(currentUvindex).html(response.value);
+            });
+}
