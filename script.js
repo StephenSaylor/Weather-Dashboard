12676339869e9ab9567deb557dd1035983cf6a53
Jeff Lebowski <anentitymusic@gmail.com>
2021-04-27 19:14:17 -0400

    adjusted uv index objects

--- script.js
+++ script.js
@@ -72,11 +72,11 @@ function currentWeather(city){
 function UVIndex(ln,lt){
 	var uvqURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lt + "&lon=" + ln;
 	$.ajax({
-			url: uvqURL,
-			method: "GET"
-			}).then(function(response){
-				$(currentUvindex).html(response.value);
-			});
+		url: uvqURL,
+		method: "GET"
+		}).then(function(response){
+			$(currentUvindex).html(response.value);
+		});
 	}
 
 function forecast(cityid){
